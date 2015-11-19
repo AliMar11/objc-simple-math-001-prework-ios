9757880bdce2ea3c7d9c13f3858c91d37d5a8bab
AliMar11 <alicia.marisal@gmail.com>
2015-11-19 16:35:41 -0500

    playing with equations!

--- objc-simple-math/FISAppDelegate.m
+++ objc-simple-math/FISAppDelegate.m
@@ -5,13 +5,184 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here.
-     
-     */
+    NSInteger i = 0 ;
+    NSLog(@"i: %li", i);
+    
+    i = 1 + 1;
+     NSLog(@"i: %li", i);
+    
+    i *= 3 ;
+     NSLog(@"i: %li", i);
+    
+    i = 17 - 5;
+     NSLog(@"i: %li", i);
+    
+    i = 20 / 10;
+     NSLog(@"i: %li", i);
+    
+    i = -20 / 10;
+     NSLog(@"i: %li", i);
+    
+    i = 4 + 2;
+     NSLog(@"i: %li", i);
+    
+    i = 3 * 5;
+     NSLog(@"i: %li", i);
+    
+    i = -45 / 3;
+     NSLog(@"i: %li", i);
+    
+    i = 1 * 2;
+     NSLog(@"i: %li", i);
+    
+    i = 9 - 6;
+     NSLog(@"i: %li", i);
+    
+    i =  0 / 1;
+     NSLog(@"i: %li", i);
+    
+    
+    NSInteger a = 0;
+    NSInteger b = 0;
+    NSInteger c = a + b;
+    
+    NSLog(@"a: %li, b: %li, c: %li", a, b, c);
+    
+    a = 17;
+    b = 29;
+    c = a + b;
+    
+    NSLog(@"a: %li, b: %li, c:%li", a, b, c);
+    
+    a = 17;
+    b = 29;
+    c = a - b;
+    
+    NSLog(@"a:%li, b:%li, c:%li", a, b, c);
+ 
+    a = 17;
+    b = 29;
+    c = a * b;
+    
+    NSLog(@"a:%li, b:%li, c:%li", a, b, c);
+    
+    a = 17;
+    b = 29;
+    c = a / b;
+    
+    NSLog(@"a:%li, b:%li, c:%li", a, b, c);
+    
+    NSUInteger u = 1;
+    NSLog(@"u:%lu", u);
+    
+    u = 2 + 3;
+    
+    NSLog(@"u:%lu", u);
+    
+    u = 2 * 3;
+    NSLog(@"u:%lu", u);
+    
+   // u = -1;
+    NSLog(@"u:%lu", u);
+    
+   // u = 8 - 10;
+    NSLog(@"u:%lu", u);
+    
+    BOOL threeIsEqualToThree = 3 == 3;
+    NSLog(@"3 == 3: %d", threeIsEqualToThree);
+    
+    BOOL fourIsEqualToThree = 4 == 3;
+    NSLog(@"fourIsEqualToThree:%d", fourIsEqualToThree);
+    
+    BOOL fiveIsNotEqualToThree = 5 != 3;
+    NSLog(@"5 != 3: %d", fiveIsNotEqualToThree);
+    
+    BOOL sixIsNotEqualToSix = 6 != 6;
+    NSLog(@"6 != 6: %d", sixIsNotEqualToSix);
+    
+    NSLog(@"7 < 8: %d", 7 < 8);
+    NSLog(@"8 < 5: %d", 8 < 5);
+    NSLog(@"9 < 9:%d", 9 < 9);
+    NSLog(@"10 <= 10: %d", 10 <= 10);
+    
+    NSLog(@"11 > 8: %d", 11 > 8);
+    NSLog(@"12 > 15: %d", 12 > 15);
+    NSLog(@"13 > 13: %d", 13 > 13);
+    NSLog(@"14 >= 14: %d", 14 >= 14);
+    
+    BOOL aIsEqualToB = a == b;
+    BOOL aIsGreaterThanB = a > b;
+    BOOL aIsLessThanB = a < b;
+    
+    NSLog(@"a == b: %d", aIsEqualToB);
+    NSLog(@"a > b:%d", aIsGreaterThanB);
+    NSLog(@"a < b:%d", aIsLessThanB);
+    
+    NSInteger x = 2 + 3 * 5;
+    NSLog(@"x:%li", x);
+    
+   // NSInteger g = 10 - 5 * 2;
+   //NSLog(@"g: %li", g);
+    
+    x = (2 + 3) * 5;
+    NSLog(@"x:%li", x);
+    
+    NSInteger y = 5 - 8 * 4 + 2;
+    NSLog(@"y: %li", y);
+    
+    y = 5 - 8 * (4 + 2);
+    NSLog(@"y: %li", y);
+    
+    CGFloat f = 0.0;
+    NSLog(@"f: %f", f);
+    
+    f = 17 / 29;
+    NSLog(@"f: %f", f);
+    
+    f = 17.0 / 29;
+    NSLog(@"f: %f", f);
+    
+    f = 1 + 2.5;
+    NSLog(@"f: %f", f);
+    f = 10 - 1.11;
+    NSLog(@"f: %f", f);
+    f = 0.1667 * 6;
+    NSLog(@"f: %f", f);
+    f = 3 / 7;
+    NSLog(@"f: %f", f);
+    f = 3 / 7.0;
+    NSLog(@"f: %f", f);
+    
+    f = M_PI;
+    NSLog(@"f: %f", f);
+    
+    f = M_PI;
+    NSLog(@"f: %.12f", f);
+    //NSLog(@"f: %.30f", f);
+    
+    f = sqrt(2);
+    NSLog(@"f: %f", f);
+    f = M_SQRT2;
+    NSLog(@"f: %f", f);
+    
+    f = sqrt(81);
+    NSLog(@"f: %f", f);
+    f = pow(3, 3);
+    NSLog(@"f: %f", f);
+
+    f = pow(2, 63) -1;
+    NSLog(@"f: %f", f);
+    //f = exp2(-64.0);
+    //NSLog(@"f: %f", f);
+    
+    f = exp2(63);
+    NSLog(@"f:%f", f);
+    
+    
+    //CGFloat fa = exp2(64.0);
+    //CGFloat fb = exp2(-64.0);
+    
     
     // do not alter
     return YES;  //
